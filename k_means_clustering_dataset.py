370af90b0d67f18925b0d036d5c64d9d2d6cf605
Corwin Smith <corwin.smith@uleth.ca>
2017-11-19 16:37:27 -0700

    Working on accessing milliion song dataset

--- k_means_clustering_dataset.py
+++ k_means_clustering_dataset.py
@@ -1,4 +1,7 @@
 from sklearn.cluster import KMeans
 import numpy as np
+import h5py
 
- 
+test = h5py.File('/Users/corwinsmith/Documents/Database_DataMining_GroupProject/playlist_recommender/MillionSongSubset/data/A/A/A/TRAAABD128F429CF47.h5', 'r')
+thing = test['analysis']['songs'].attrs.items()[6]
+print test['analysis']['songs'].attrs.__dict__[0]
