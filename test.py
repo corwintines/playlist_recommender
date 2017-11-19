e7ce81ba4b5fb2b27622e85cbc9be7ebd05a195f
Corwin Smith <corwin.smith@uleth.ca>
2017-11-19 13:35:43 -0700

    Merge conflicts resolved

--- test.py
+++ test.py
@@ -1,17 +1,12 @@
 from Playlist import Playlist
 from TrainingPlaylist import TrainingPlaylist
 
-<<<<<<< HEAD
 # p = Playlist('spotify','37i9dQZF1DWZeKCadgRdKQ')
-=======
 # p = Playlist('spotify','37i9dQZF1DX76Wlfdnj7AP')
->>>>>>> master
 # p.get_playlist()
 # p.generate_playlist_vector()
 # print p.playlist_attribute_vector
 
-<<<<<<< HEAD
-=======
 t = TrainingPlaylist('spotify','37i9dQZF1DX76Wlfdnj7AP')
 t.get_playlist()
 t.construct_training_groundtruths()
@@ -19,4 +14,3 @@ t.generate_playlist_vector()
 print t.training_attribute_vector
 t.find_cluster()
 t.trim_outliers()
->>>>>>> master
