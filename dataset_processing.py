79e0ab6a9750957efa1a0dc06e0ca0574610e58c
Corwin Smith <corwin.smith@uleth.ca>
2017-11-20 10:01:23 -0700

    Query spotify for attributes and construct list for every song complete

--- dataset_processing.py
+++ dataset_processing.py
@@ -7,19 +7,32 @@ def query_spotify_for_attributes():
     MSD = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'MillionSongSubset', 'data')
     MSD_song_list = getMSDSongTitleAndArtistList(MSD)
     MSD_song_attributes = []
-    print(len(MSD_song_list))
+    MSD_song_final = []
 
     for song in range(0, 100):
+        print song
         data = {'artist': MSD_song_list[song][1], 'songname': MSD_song_list[song][0]}
         attributes = requests.get('https://playlist-recommender.herokuapp.com/spotify_tracks', data)
         uri = attributes.text
-        if uri is 'oops':
-            MSD_song_list.pop(song)
+        if uri == 'oops':
+            MSD_song_attributes.append(None)
         else:
-            uri = attributes.text.split(':')[2]
+            uri = uri.split(':')[2]
             data = {'song_uri': uri}
             attributes = requests.get('https://playlist-recommender.herokuapp.com/get_track_attributes', data)
+            print(attributes)
             attributes = attributes.json()
             MSD_song_attributes.append(attributes)
 
-    print(len(MSD_song_attributes))
+    for data in range(0, len(MSD_song_attributes)):
+        if MSD_song_attributes[data] != None:
+            MSD_song_final.append([
+            MSD_song_list[data][0],
+            MSD_song_list[data][1],
+            MSD_song_attributes[data][0],
+            MSD_song_attributes[data][1],
+            MSD_song_attributes[data][2],
+            MSD_song_attributes[data][3]
+            ])
+
+    return MSD_song_final
