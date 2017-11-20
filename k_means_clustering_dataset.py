540f2669bf6eb07aef2f1a41a0166eb415c1bbdc
Corwin Smith <corwin.smith@uleth.ca>
2017-11-19 19:32:13 -0700

    K-means functions should be done, just need the data to go with them

--- k_means_clustering_dataset.py
+++ k_means_clustering_dataset.py
@@ -2,6 +2,8 @@ from sklearn.cluster import KMeans
 import numpy as np
 import h5py
 
-test = h5py.File('/Users/corwinsmith/Documents/Database_DataMining_GroupProject/playlist_recommender/MillionSongSubset/data/A/A/A/TRAAABD128F429CF47.h5', 'r')
-thing = test['analysis']['songs'].attrs.items()[6]
-print test['analysis']['songs'].attrs.__dict__[0]
+
+attributes = np.array()
+kmeans = KMeans(n_clusters=2, random_state=0).fit(attributes)
+labels = kmeans.predict(attributes)
+centroids = kmeans.cluster_centers_
