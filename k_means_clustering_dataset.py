6385a352e4d66ea29fa6de7a4252c6984524cfa3
Corwin Smith <corwin.smith@uleth.ca>
2017-11-19 19:58:27 -0700

    Comment

--- k_means_clustering_dataset.py
+++ k_means_clustering_dataset.py
@@ -3,7 +3,8 @@ import numpy as np
 import h5py
 
 
+# Takes the array of attributes for songs and clusters them
 attributes = np.array()
-kmeans = KMeans(n_clusters=2, random_state=0).fit(attributes)
+kmeans = KMeans(n_clusters=10, random_state=0).fit(attributes)
 labels = kmeans.predict(attributes)
 centroids = kmeans.cluster_centers_
