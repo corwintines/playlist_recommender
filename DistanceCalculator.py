d86cf44f00d0286bba64454a009301064fcdc4cf
Yash Patel <Yash@Yashs-MacBook-Air.local>
2017-11-26 16:44:22 -0700

    pearson correlation

--- DistanceCalculator.py
+++ DistanceCalculator.py
@@ -117,11 +117,49 @@ def pearsonCorrelation(songAttributes):
 					l = l+1
 	return correlationMatrix
 
+def pearsonCorrelationDict(songAttributes):
+	numOfElements = (len(songAttributes))
+	sizeOfElement = (len(songAttributes[0]))
 
+	Danceability = []
+	Energy = []
+	Loudness = []
+	Tone = []
+	Beats = []
+
+	for i in range(0, (len(songAttributes[0]))):
+		for j in range(0, (len(songAttributes))):
+			if i == 0:
+				Danceability.append(songAttributes[j].__getitem__(i))
+			if i == 1:
+				Energy.append(songAttributes[j].__getitem__(i))
+			if i == 2:
+				Loudness.append(songAttributes[j].__getitem__(i))
+			if i == 3:
+				Tone.append(songAttributes[j].__getitem__(i))
+			if i ==4:
+				Beats.append(songAttributes[j].__getitem__(i))
+
+	print "Danceability: ", Danceability
+	print "Energy: ", Energy
+	print "Loudness: ", Loudness
+	print "Tone: ", Tone
+	print "Beats: ", Beats
+
+	dict = {'Danceability' : {'Energy':(scipy.stats.pearsonr(Danceability,Energy)[0]), 'Loudness':(scipy.stats.pearsonr(Danceability,Loudness)[0]), 'Tone':(scipy.stats.pearsonr(Danceability,Tone)[0]), 'Beats':(scipy.stats.pearsonr(Danceability,Beats)[0])},
+			'Energy' : {'Danceability':(scipy.stats.pearsonr(Danceability,Energy)[0]), 'Loudness':(scipy.stats.pearsonr(Energy,Loudness)[0]), 'Tone':(scipy.stats.pearsonr(Energy,Tone)[0]), 'Beats':(scipy.stats.pearsonr(Energy,Beats[0]))},
+			'Loudness' : {'Danceability': (scipy.stats.pearsonr(Loudness,Danceability)[0]), 'Energy':(scipy.stats.pearsonr(Loudness,Energy)[0]), 'Tone':(scipy.stats.pearsonr(Loudness,Tone)[0]), 'Beats':(scipy.stats.pearsonr(Loudness,Beats)[0])},
+			'Tone' : {'Danceability':(scipy.stats.pearsonr(Tone,Danceability)[0]), 'Energy':(scipy.stats.pearsonr(Tone,Energy)[0]), 'Loudness':(scipy.stats.pearsonr(Tone,Loudness)[0]), 'Beats':(scipy.stats.pearsonr(Tone,Beats)[0])},
+			'Beats' : {'Danceability':(scipy.stats.pearsonr(Beats, Danceability)[0]), 'Energy':(scipy.stats.pearsonr(Beats,Energy[0]), 'Loudness':(scipy.stats.pearsonr(Beats,Loudness)[0]), 'Tone':(scipy.stats.pearsonr(Beats,Tone)[0])
+	print 'Danceability :', dict['Danceability']
+	print 'Energy :', dict['Energy']
+	print 'Loudness :', dict['Loudness']
+	print 'Tone :', dict['Tone']
+	print 'Beats :', dict['Beats']
 
 	#print str(i), locals()["group" + str(i)]
 	#return scipy.stats.pearsonr(listOfSongs, aggregateVector)
 
 
 cluster_data(listOfCentroids)
-print pearsonCorrelation(songAttributes)
+print pearsonCorrelationDict(songAttributes)
