c298f948e5b7f1f52a27386910c12c92c54515dd
Lucas Jakober <lucasjakober@lucas.local>
2017-11-26 17:36:37 -0700

    encoding

--- cred.py
+++ cred.py
@@ -1,11 +1,15 @@
 import base64
+
+########## Fill in your username and password for Luke's Macbook #############
+username = base64.b64encode('')
+password = base64.b64encode('')
+
+
+
 addr = base64.b64encode('10.76.136.154')
 addr_port = base64.b64encode('22')
 remoteAddr = base64.b64encode('127.0.0.1')
 remoteAddr_port = base64.b64encode('3306')
-username = base64.b64encode('lucasjakober')
-password = base64.b64encode('FordF150$')
-
 
 def getUsername():
 	return username
