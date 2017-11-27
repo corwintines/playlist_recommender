624884d087744e85631d83e59d4d362e5c8f4822
Lucas Jakober <lucasjakober@lucas.local>
2017-11-26 21:41:02 -0700

    set loop for 500 songs from MSD for feature selection testing

--- sql_connector.py
+++ sql_connector.py
@@ -52,6 +52,57 @@ def openCursor(connection):
 def closeCursor(cursor):
 	cursor.close()
 
+def createClusterTable(cursor):
+	query = (
+		"CREATE TABLE Cluster "
+		"("
+		"ClusterID int(11) NOT NULL, "
+		"Danceability decimal(12,8) NOT NULL, "
+		"Energy decimal(12,8) NOT NULL, "
+		"Loudness decimal(12,8) NOT NULL, "
+		"Tempo decimal(12,8) NOT NULL, "
+		"PRIMARY KEY (ClusterID) "
+		") "
+		"ENGINE=InnoDB DEFAULT CHARSET=utf8; "
+		)
+	cursor.execute(query)
+
+def createSongTable(cursor):
+	query = (
+		"CREATE TABLE Song "
+		"("
+		"song_DB_ID int(11) NOT NULL AUTO_INCREMENT, "
+		"ClusterID int(11) NOT NULL, "
+		"Title varchar(255) NOT NULL, "
+		"Artist_Name varchar(255) NOT NULL, "
+		"Artist_Familiarity decimal(12,8) NOT NULL, "
+		"Artist_Hotness decimal(12,8) NOT NULL, "
+		"Danceability decimal(12,8) NOT NULL, "
+		"Duration decimal(12,8) NOT NULL, "
+		"End_of_Fade_In decimal(12,8) NOT NULL, "
+		"Energy decimal(12,8) NOT NULL, "
+		"Loudness decimal(12,8) NOT NULL, "
+		"Start_of_Fade_Out decimal(12,8) NOT NULL, "
+		"Tempo decimal(12,8) NOT NULL, "
+		"PRIMARY KEY (song_DB_ID), "
+		"KEY Songs_ClusterID_INDEX (ClusterID)"
+		") "
+		"ENGINE=InnoDB AUTO_INCREMENT=86137 DEFAULT CHARSET=utf8; "
+		)
+	cursor.execute(query)
+
+def addSongToDB(cursor, songTuple):
+	query = (
+		"INSERT INTO Songs "
+		"(ClusterID, "
+		"Title, "
+		"Artist_Name, "
+		"Artist_Familiarity, "
+		"Danceability, Energy, Loudness, Tempo) "
+		""
+		)
+
+
 def addSongToDB(cursor, songAttrTup):
 	# add_song_SQL = ("INSERT INTO Songs "
 	# 	"(ClusterID, Title, Artist, Danceability, Energy, Loudness, Tempo) "
@@ -75,22 +126,23 @@ def main():
 		conn = mysql.connector.connect(host='127.0.0.1', port=server.local_bind_port, database='Song_DB', user='yashpatel', passwd='yashpatel')
 		cursor = conn.cursor()
 		
+		createSongTable(cursor)
 		# login(config)
 		# cnx = openConnection(config)
 		# cursor = openCursor(cnx)
-		counter = 0
-		while counter < 100:
-			song1 = (1,'Doctor Jones','Aqua',4.782,1000.004444,-999.99,188)
-			# song1_Clean = "VALUES {0}, {1}, {2}, {3:.4f}, {4:.4f}, {5:.4f}, {6:.4f}".format(song1[0],song1[1],song1[2],song1[3],song1[4],song1[5],song1[6])
-			song2 = (2,'My Heart Will Go On','Celine Dion',4.782,1000.004444,-999.99,188)
-			# song2_Clean = "{0}, {1}, {2}, {4}8.4f, {8.4f}, {8.4f}, {8.4f}".format(song2,)
-			addSongToDB(cursor, song1)
-			conn.commit()
-			addSongToDB(cursor, song2)
-			conn.commit()
-			counter +=1
-		print song1
-		print song2
+		# counter = 0
+		# while counter < 100:
+		# 	song1 = (1,'Doctor Jones','Aqua',4.782,1000.004444,-999.99,188)
+		# 	# song1_Clean = "VALUES {0}, {1}, {2}, {3:.4f}, {4:.4f}, {5:.4f}, {6:.4f}".format(song1[0],song1[1],song1[2],song1[3],song1[4],song1[5],song1[6])
+		# 	song2 = (2,'My Heart Will Go On','Celine Dion',4.782,1000.004444,-999.99,188)
+		# 	# song2_Clean = "{0}, {1}, {2}, {4}8.4f, {8.4f}, {8.4f}, {8.4f}".format(song2,)
+		# 	addSongToDB(cursor, song1)
+		# 	conn.commit()
+		# 	addSongToDB(cursor, song2)
+		# 	conn.commit()
+		# 	counter +=1
+		# print song1
+		# print song2
 		# print song1_Clean
 		# print song2_Clean
 
