e2ff55bbfff6a19727884693e684e09c7d9b349b
Lucas Jakober <lucasjakober@lucas.local>
2017-11-27 22:43:30 -0700

    More sql for database evaluation purposes.

--- sql_connector.py
+++ sql_connector.py
@@ -4,6 +4,9 @@ import time
 import base64
 from sshtunnel import SSHTunnelForwarder
 import cred
+import pickle
+import random
+import decimal
 
 '''
 To access the database:
@@ -52,7 +55,7 @@ class Song_DB:
 				database='Song_DB', 
 				user=base64.b64decode(cred.getUsername_MySQL()), 
 				passwd=base64.b64decode(cred.getPassword_MySQL()))
-			self.cursor = self.connection.cursor()
+			self.cursor = self.connection.cursor(buffered=True)
 			print "open connection: success\nopen cursor: success"
 			return self
 		except mysql.connector.Error as e:
@@ -108,43 +111,75 @@ class Song_DB:
 				"{2:.8f}, "
 				"{3:.8f}, "
 				"{4:.8f}, "
-				"{5:.8f}, "
-				"{6:.8f}, "
-				"{7:.8f}, "
-				"{8:.8f}, "
-				"{9:.8f}, "
-				"{10:.8f}, "
-				"{11:.8f}, "
-				"{12:.8f}, "
-				"{13:.8f} "
+				"{5:.8f} "
+				# "{6:.8f}, "
+				# "{7:.8f}, "
+				# "{8:.8f}, "
+				# "{9:.8f}, "
+				# "{10:.8f}, "
+				# "{11:.8f}, "
+				# "{12:.8f}, "
+				# "{13:.8f} "
 				" ) ;".format(
 				cluster[0],
 				cluster[1],
 				cluster[2],
 				cluster[3],
 				cluster[4],
-				cluster[5],
-				cluster[6],
-				cluster[7],
-				cluster[8],
-				cluster[9],
-				cluster[10],
-				cluster[11],
-				cluster[12],
-				cluster[13])
+				cluster[5])
+				# cluster[6],
+				# cluster[7],
+				# cluster[8],
+				# cluster[9],
+				# cluster[10],
+				# cluster[11],
+				# cluster[12],
+				# cluster[13])
 				)
+			# values = (
+			# 	"VALUES ("
+			# 	"{0}, "
+			# 	"{1:.8f}, "
+			# 	"{2:.8f}, "
+			# 	"{3:.8f}, "
+			# 	"{4:.8f}, "
+			# 	"{5:.8f}, "
+			# 	"{6:.8f}, "
+			# 	"{7:.8f}, "
+			# 	"{8:.8f}, "
+			# 	"{9:.8f}, "
+			# 	"{10:.8f}, "
+			# 	"{11:.8f}, "
+			# 	"{12:.8f}, "
+			# 	"{13:.8f} "
+			# 	" ) ;".format(
+			# 	cluster[0],
+			# 	cluster[1],
+			# 	cluster[2],
+			# 	cluster[3],
+			# 	cluster[4],
+			# 	cluster[5],
+			# 	cluster[6],
+			# 	cluster[7],
+			# 	cluster[8],
+			# 	cluster[9],
+			# 	cluster[10],
+			# 	cluster[11],
+			# 	cluster[12],
+			# 	cluster[13])
+			# 	)
 			query = columns + values
 			if (self.executeQuery(query,'Error adding Cluster to DB')):	
 				self.connection.commit()
 				added_to_db += 1
 		print "successfully added %d of %d clusters in cluster_list to DB"%(added_to_db,len(cluster_list))
 
-	def insert_Songs(self, song_list=[]):
+	def insert_Song_Clustered_Index(self, song_list=[]):
 		if not song_list:
 			print "song_list is empty"
 			return
 		columns = (
-			"INSERT INTO Song "
+			"INSERT INTO Song_Clustered_Index "
 			"(ClusterID, "
 			"Title, "
 			"Artist_Name, "
@@ -201,7 +236,74 @@ class Song_DB:
 				song[15])
 				)
 			query = columns + values
-			if (self.executeQuery(query,'Error adding Song to DB')):
+			if (self.executeQuery(query,'Error adding song to DB')):
+				self.connection.commit()
+				added_to_db += 1
+		print "successfully added %d of %d songs in song_list to DB"%(added_to_db,len(song_list))
+
+	def insert_Song_No_Index(self, song_list=[]):
+		if not song_list:
+			print "song_list is empty"
+			return
+		columns = (
+			"INSERT INTO Song_No_Index "
+			"(ClusterID, "
+			"Title, "
+			"Artist_Name, "
+			"Accousticness, "
+			"Artist_Familiarity, "
+			"Artist_Hotness, "
+			"Danceability, "
+			"Duration, "
+			"End_of_Fade_In, "
+			"Energy, "
+			"Instrumentalness, "
+			"Loudness, "
+			"Speechiness, "
+			"Start_of_Fade_Out, "
+			"Tempo, "
+			"Valence) "
+			)
+		added_to_db = 0
+		for song in song_list:	
+			values = (
+				"VALUES ("
+				"{0}, "
+				"'{1}', "
+				"'{2}', "
+				"{3:.8f}, "
+				"{4:.8f}, "
+				"{5:.8f}, "
+				"{6:.8f}, "
+				"{7:.8f}, "
+				"{8:.8f}, "
+				"{9:.8f}, "
+				"{10:.8f}, "
+				"{11:.8f}, "
+				"{12:.8f}, "
+				"{13:.8f}, "
+				"{14:.8f}, "
+				"{15:.8f} "
+				" ) ;".format(
+				song[0],
+				song[1],
+				song[2],
+				song[3],
+				song[4],
+				song[5],
+				song[6],
+				song[7],
+				song[8],
+				song[9],
+				song[10],
+				song[11],
+				song[12],
+				song[13],
+				song[14],
+				song[15])
+				)
+			query = columns + values
+			if (self.executeQuery(query,'Error adding song to DB')):
 				self.connection.commit()
 				added_to_db += 1
 		print "successfully added %d of %d songs in song_list to DB"%(added_to_db,len(song_list))
@@ -214,24 +316,24 @@ class Song_DB:
 			print errorDescription+": "+str(e)
 			return False
 
-	def create_ClusterTable(self):
+	def createTable_Cluster(self):
 		query = (
 			"CREATE TABLE Cluster "
 			"("
 			"ClusterID int(11) NOT NULL, "
 			"Accousticness decimal(12,8) NOT NULL, "
-			"Artist_Familiarity decimal(12,8) NOT NULL, "
-			"Artist_Hotness decimal(12,8) NOT NULL, "
-			"Danceability decimal(12,8) NOT NULL, "
-			"Duration decimal(12,8) NOT NULL, "
-			"End_of_Fade_In decimal(12,8) NOT NULL, "
+			# "Artist_Familiarity decimal(12,8) NOT NULL, "
+			# "Artist_Hotness decimal(12,8) NOT NULL, "
+			# "Danceability decimal(12,8) NOT NULL, "
+			# "Duration decimal(12,8) NOT NULL, "
+			# "End_of_Fade_In decimal(12,8) NOT NULL, "
 			"Energy decimal(12,8) NOT NULL, "
 			"Instrumentalness decimal(12,8) NOT NULL, "
 			"Loudness decimal(12,8) NOT NULL, "
 			"Speechiness decimal(12,8) NOT NULL, "
-			"Start_of_Fade_Out decimal(12,8) NOT NULL, "
-			"Tempo decimal(12,8) NOT NULL, "
-			"Valence decimal(12,8) NOT NULL, "
+			# "Start_of_Fade_Out decimal(12,8) NOT NULL, "
+			# "Tempo decimal(12,8) NOT NULL, "
+			# "Valence decimal(12,8) NOT NULL, "
 			"PRIMARY KEY (ClusterID) "
 			") "
 			"ENGINE=InnoDB DEFAULT CHARSET=utf8; "
@@ -239,9 +341,9 @@ class Song_DB:
 		if self.executeQuery(query,'Error creating Cluster table in DB'):
 			print "Cluster table added."
 
-	def create_SongTable(self):
+	def createTable_Song_Clustered_Index(self):
 		query = (
-			"CREATE TABLE Song "
+			"CREATE TABLE Song_Clustered_Index "
 			"("
 			"song_DB_ID int(11) NOT NULL AUTO_INCREMENT, "
 			"ClusterID int(11) NOT NULL, "
@@ -268,8 +370,43 @@ class Song_DB:
 		if self.executeQuery(query,'Error creating Song table in DB'):
 			print "Song table added."
 
+	def createTable_Song_No_Index(self):
+		query = (
+			"CREATE TABLE Song_No_Index "
+			"("
+			"song_DB_ID int(11) NOT NULL AUTO_INCREMENT, "
+			"ClusterID int(11) NOT NULL, "
+			"Title varchar(255) NOT NULL, "
+			"Artist_Name varchar(255) NOT NULL, "
+			"Accousticness decimal(12,8) NOT NULL, "
+			"Artist_Familiarity decimal(12,8) NOT NULL, "
+			"Artist_Hotness decimal(12,8) NOT NULL, "
+			"Danceability decimal(12,8) NOT NULL, "
+			"Duration decimal(12,8) NOT NULL, "
+			"End_of_Fade_In decimal(12,8) NOT NULL, "
+			"Energy decimal(12,8) NOT NULL, "
+			"Instrumentalness decimal(12,8) NOT NULL, "
+			"Loudness decimal(12,8) NOT NULL, "
+			"Speechiness decimal(12,8) NOT NULL, "
+			"Start_of_Fade_Out decimal(12,8) NOT NULL, "
+			"Tempo decimal(12,8) NOT NULL, "
+			"Valence decimal(12,8) NOT NULL, "
+			"PRIMARY KEY (song_DB_ID) "
+			") "
+			"ENGINE=InnoDB AUTO_INCREMENT=86137 DEFAULT CHARSET=utf8; "
+			)
+		if self.executeQuery(query,'Error creating Song table in DB'):
+			print "Song table added."
+	
 	def delete_ALL_SONGS_IN_DB(self):
 		query = "TRUNCATE Song;"
+		if self.executeQuery(query,'Error deleting all songs from Song table in DB'):
+			print "All songs in Song table deleted."
+		query = "TRUNCATE Song_No_Index;"
+		if self.executeQuery(query,'Error deleting all songs from Song table in DB'):
+			print "All songs in Song table deleted."
+		
+		query = "TRUNCATE Song_Clustered_Index;"
 		if self.executeQuery(query,'Error deleting all songs from Song table in DB'):
 			print "All songs in Song table deleted."
 
@@ -278,26 +415,55 @@ class Song_DB:
 		if self.executeQuery(query,'Error deleting all clusters from Cluster table in DB'):
 			print "All clusters in Cluster table deleted."
 
+	def select_Song_By_ClusterID(self, tableName, clusterID):
+		query = "SELECT * FROM "+tableName+" WHERE ClusterID="+str(clusterID)+";"
+		if self.executeQuery(query,'Error selecting songs by ClusterID'):
+			print "Select successful."
 
-cluster_list = [
-	(1,0.342345,0.5345,6345.346,0.34634,2643.346,2456.243464,0.23456234,0.2346,0.2346,0.2346,0.2346,0.2346,0.2346),
-	(2,0.342345,0.5345,6345.346,0.34634,2643.346,2456.243464,0.23456234,0.2346,0.2346,0.2346,0.2346,0.2346,0.2346),
-	(3,0.342345,0.5345,6345.346,0.34634,2643.346,2456.243464,0.23456234,0.2346,0.2346,0.2346,0.2346,0.2346,0.2346)]
 
-song_list = [
-	(3,'test_song_title','test_artist',0.342345,0.5345,6345.346,0.34634,2643.346,2456.243464,0.23456234,0.2346,0.2346,0.2346,0.2346,0.2346,0.2346),
-	(3,'test_song_title','test_artist',0.342345,0.5345,6345.346,0.34634,2643.346,2456.243464,0.23456234,0.2346,0.2346,0.2346,0.2346,0.2346,0.2346),
-	(3,'test_song_title','test_artist',0.342345,0.5345,6345.346,0.34634,2643.346,2456.243464,0.23456234,0.2346,0.2346,0.2346,0.2346,0.2346,0.2346)]
+# cluster_list = [
+# 	(1,0.342345,0.5345,6345.346,0.34634,2643.346,2456.243464,0.23456234,0.2346,0.2346,0.2346,0.2346,0.2346,0.2346),
+# 	(2,0.342345,0.5345,6345.346,0.34634,2643.346,2456.243464,0.23456234,0.2346,0.2346,0.2346,0.2346,0.2346,0.2346),
+# 	(3,0.342345,0.5345,6345.346,0.34634,2643.346,2456.243464,0.23456234,0.2346,0.2346,0.2346,0.2346,0.2346,0.2346)]
 
-with Song_DB() as dbase:
-	dbase.create_SongTable()
-	dbase.create_ClusterTable()
+# song_list = [
+# 	(3,'test_song_title','test_artist',0.342345,0.5345,6345.346,0.34634,2643.346,2456.243464,0.23456234,0.2346,0.2346,0.2346,0.2346,0.2346,0.2346),
+# 	(3,'test_song_title','test_artist',0.342345,0.5345,6345.346,0.34634,2643.346,2456.243464,0.23456234,0.2346,0.2346,0.2346,0.2346,0.2346,0.2346),
+# 	(3,'test_song_title','test_artist',0.342345,0.5345,6345.346,0.34634,2643.346,2456.243464,0.23456234,0.2346,0.2346,0.2346,0.2346,0.2346,0.2346)]
 
-	dbase.insert_Songs(song_list)
-	dbase.insert_Clusters(cluster_list)
+# with Song_DB() as dbase:
+	# dbase.createTable_Song_Clustered_Index()
+	# dbase.createTable_Cluster()
+
+	# dbase.insert_Song_Clustered_Index(song_list)
+	# dbase.insert_Clusters(cluster_list)
 
 	# dbase.delete_ALL_CLUSTERS_IN_DB()
 	# dbase.delete_ALL_SONGS_IN_DB()
+	
+	# decimal.Decimal(random.randrange(10000))/100
+
+
+	# with open('centroids.txt', 'rb') as f:
+	# 	cluster_list = pickle.load(f)
+	# 	dbase.insert_Clusters(cluster_list)
+
+	# with open('all_songs_cluster_combined.txt', 'rb') as f:
+	# 	song_list = pickle.load(f)
+	# 	for song in song_list:
+	# 		song[1] = song[1].replace("'","")
+	# 		song[2] = song[2].replace("'","")
+		
+
+
+
+
+
+
+
+
+
+
 
 
 
