f926996485c3564b61122d9dbbb707b0722a62ff
Corwin Smith <corwin.smith@uleth.ca>
2017-11-27 17:54:43 -0700

    All song data

--- dataset_processing.py
+++ dataset_processing.py
@@ -9,10 +9,12 @@ def query_spotify_for_attributes():
     for song in MSD_song_list:
         if song[0] == None or song[1] == None or song[2] == None or song[3] == None or song[4] == None or song[5] == None or song[6] == None:
             MSD_song_list.remove(song)
+    print MSD_song_list[9852]
     MSD_song_attributes = []
     MSD_song_final = []
 
-    for song in range(0, 100):
+    for song in range(9854, len(MSD_song_list)):
+        print song
         data = {'artist': MSD_song_list[song][1], 'songname': MSD_song_list[song][0]}
         attributes = requests.get('https://playlist-recommender.herokuapp.com/spotify_tracks', data)
         uri = attributes.text
@@ -22,20 +24,27 @@ def query_spotify_for_attributes():
             uri = uri.split(':')[2]
             data = {'song_uri': uri}
             attributes = requests.get('https://playlist-recommender.herokuapp.com/get_track_attributes', data)
-            attributes = attributes.json()
-            MSD_song_attributes.append(attributes)
+            print attributes
+            print attributes.text
+            if attributes.status_code == 503:
+                break
+            try:
+                attributes = attributes.json()
+                MSD_song_attributes.append(attributes)
+            except:
+                MSD_song_attributes.append(None)
 
     # (title, artist, artist_familiarity, artist_hotness,duration,endOfFadeIn, startOfFadeOut, acousticness, dancability, energy, intrumentalness, loudness, speechiness, tempo, valence)
     for data in range(0, len(MSD_song_attributes)):
         if MSD_song_attributes[data] != None:
             MSD_song_final.append([
-            MSD_song_list[data][0],
-            MSD_song_list[data][1],
-            MSD_song_list[data][2],
-            MSD_song_list[data][3],
-            MSD_song_list[data][4],
-            MSD_song_list[data][5],
-            MSD_song_list[data][6],
+            MSD_song_list[9854+data][0],
+            MSD_song_list[9854+data][1],
+            MSD_song_list[9854+data][2],
+            MSD_song_list[9854+data][3],
+            MSD_song_list[9854+data][4],
+            MSD_song_list[9854+data][5],
+            MSD_song_list[9854+data][6],
             MSD_song_attributes[data][0],
             MSD_song_attributes[data][1],
             MSD_song_attributes[data][2],
