88b3c6b6c951f033f36249bc0110285048fba325
Corwin Smith <corwin.smith@uleth.ca>
2017-12-01 19:41:32 -0700

    Neural network

--- playlist_neuralnetwork_training.py
+++ playlist_neuralnetwork_training.py
@@ -4,19 +4,39 @@ from nimblenet.learning_algorithms import RMSprop
 from nimblenet.data_structures import Instance
 from nimblenet.neuralnet import NeuralNet
 from TrainingPlaylist import TrainingPlaylist
+import os
+import pickle
+
+
+def produce_dataset(training_file):
+    training_data = []
+    with open(training_file) as data:
+        dataset = pickle.load(data)
+        for song in dataset:
+            attributes = [song.attributes['accousticness'],
+                          song.attributes['danceability'],
+                          song.attributes['energy'],
+                          song.attributes['instrumentalness'],
+                          song.attributes['loudness'],
+                          song.attributes['speechiness'],
+                          song.attributes['tempo'],
+                          song.attributes['valence']]
+            recommended_value = [song.attributes['rec_value']]
+
+            training_data.append(Instance(attributes, recommended_value))
+
+    return training_data
 
 
 def main():
-    playlist = TrainingPlaylist('spotify', '37i9dQZF1DX76Wlfdnj7AP')
-    playlist.get_playlist()
-    playlist.construct_training_groundtruths()
-    training_dataset = [Instance( [0,0], [0] ), Instance( [1,0], [1] ), Instance( [0,1], [1] ), Instance( [1,1], [0] )]
+    training_dataset = produce_dataset(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'training_data', 'pickle', 'training_playlists', 'training_1.txt'))
+    test_dataset = produce_dataset(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'training_data', 'pickle', 'test_playlists', 'test_1.txt'))
 
     settings = {
         # Required settings
         "n_inputs": 8, # Number of input signals
         "layers": [
-                    (8, sigmoid_function), # First input layer (number of nodes, activation function)
+                    (8, sigmoid_function), # First Hidden Layer (number of nodes, activation function)
                     (1, sigmoid_function)  # Output layer
                 ],
 
@@ -27,11 +47,12 @@ def main():
     }
 
     network = NeuralNet(settings)
-    training_set = dataset
-    test_set = dataset
+    training_set = training_dataset
+    test_set = test_dataset
+    cost_function = cross_entropy_cost
 
-    RMSprop(network, training_set, test_set, cost_function, ERROR_LIMIT=0.1)
-    network.save_network_to_file("%s.pkl" % "filename")
+    RMSprop(network, training_set, test_set, cost_function, ERROR_LIMIT=0.1, max_iterations=100000, batch_size=1800)
+    network.save_network_to_file("%s.pkl" % "filename3")
 
 
 main()
